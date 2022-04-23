fd2268f277617f995d06f28125fb6c0a965d0229
Mike Addams <mj_adamski@wp.pl>
2022-04-23 13:37:33 +0200

    first step into java game dev, setting up, initialize window

---
+++ src/main/java/Jade/Window.java
@@ -0,0 +1,69 @@
+package Jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private int width,height;
+    private String title;
+    private long glfwWindow;
+    private static Window window = null;
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Mario";
+
+    }
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("hello LWJGL "+ Version.getVersion() + "|");
+        init();
+        loop();
+    }
+    public void init() {
+        //setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+        //initialize GLW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initalize GLFW.");
+        }
+        //Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+        //Create the window
+        glfwWindow = glfwCreateWindow(this.width,this.height, this.title,NULL,NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create GLFW window.");
+        }
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        // Enable v-sync
+        glfwSwapInterval(1);
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
+        GL.createCapabilities();
+    }
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // Poll events
+            glfwPollEvents();
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
